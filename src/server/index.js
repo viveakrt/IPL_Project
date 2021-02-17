b08893b3a3d784a4465f8182eee1ab751a49dd0b
Vivek <vivek.15@mountblue.tech>
2021-02-17 17:34:42 +0530

    Added numOfWins fuction

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,41 @@
-const ipl = require('./ipl');
+const ipl = require('./ipl.js');
 const csv = require('csv-parser');
+const path = require('path');
 const fs = require('fs');
 const results = [];
+const resultDeliveries = [];
+const src = path.join(__dirname,'../') + '/public/output/';
 
 fs.createReadStream('../data/matches.csv')
 .pipe(csv())
 .on('data', (data) => results.push(data))
 .on('end', () => {
-    console.log(JSON.stringify(ipl.numOfMatches(results)));
 
+    fs.writeFile(src + 'matchesPerYear.json', JSON.stringify(ipl.numOfMatches(results)) , 'utf8',(err) => {
+        if(err) {
+            console.log(err);
+        }
+    });
+
+    fs.writeFile(src + 'matchesWonPerYear.json', JSON.stringify(ipl.numOfWins(results)) , 'utf8',(err) => {
+        if(err) {
+            console.log(err);
+        }
+    });
+
+    fs.createReadStream('../data/deliveries.csv')
+    .pipe(csv())
+    .on('data', (data) => resultDeliveries.push(data))
+    .on('end', () => {
+
+        fs.writeFile(src + 'extraRunsPerTeam.json', JSON.stringify(ipl.extraRunPerTeam(results,resultDeliveries)) , 'utf8',(err) => {
+            if(err) {
+                console.log(err);
+            }
+        });
+
+    }
+
+    console.log();
 });
 
--- src/server/ipl.js
+++ src/server/ipl.js
@@ -1,24 +1,54 @@
-function numOfMatches(matches){
-
-    if (typeof matches !== 'object' || matches.length === 0){
+function numOfMatches(matches) {
+    if (typeof matches !== 'object' || matches.length === 0) {
         return {};
-    }
-    else{
+    } else {
         let yearCount = {};
 
-        for (let index=0; index < matches.length; index++) {
-            let years=matches[index].season;
-            if(years in yearCount){
+        for (let index = 0; index < matches.length; index++) {
+            let years = matches[index].season;
+            if (years in yearCount) {
                 yearCount[years]++;
-            }
-            else{
+            } else {
                 yearCount[years] = 1;
             }
         }
         return yearCount;
+
     }
 }
 
-module.exports = {
-    numOfMatches
+function numOfWins(matches) {
+    if (typeof matches !== 'object' || matches.length === 0){
+        return {};
+    }
+    else{
+        let teams = {};
+        for (let index=0; index < matches.length; index++) {
+            let season=matches[index].season;
+            let winner=matches[index].winner;
+            if (teams[season] != undefined) {
+                
+                if (teams[season][winner] != undefined) {
+                    teams[season][winner] += 1;
+                } else {
+                    teams[season][winner] = 1;
+                }
+            } else {
+                teams[season] = {};
+                teams[season][winner] = 1;
+            }
+        }
+        return teams;
+    }
 }
+
+function extraRunPerTeam(){
+
+}
+
+
+module.exports = {
+    numOfMatches,
+    numOfWins,
+    extraRunPerTeam
+};
