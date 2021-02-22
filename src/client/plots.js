70510a15bef8f383352b243c8a0111de7e29f838
Vivek <vivek.15@mountblue.tech>
2021-02-22 12:07:28 +0530

    Added chart for matches per year in plot.js from output data and in css added padding

--- src/client/plots.js
+++ src/client/plots.js
@@ -1,8 +1,93 @@
 fetch('./api/extras')
     .then(res => res.json())
+    .then(extraRunsPerTeam);
+
+fetch('./api/match')
+    .then(res => res.json())
     .then(matchesPerYear);
 
-function matchesPerYear(data) {
+fetch('./api/matchwon')
+    .then(res => res.json())
+    .then(matchesWonPerYear);
+
+function matchesWonPerYear(data){
+    
+}
+
+
+function matchesPerYear(data){
+    let seriesData = [];
+    for (let d in data) {
+        seriesData.push([d, data[d]]);
+    }
+
+    Highcharts.chart('container2', {
+        chart: {
+            type: 'column',
+        },
+        title: {
+            text: 'Matches Per Year',
+            style:{
+                fontSize:'22px',
+                fontWeight:'bold',
+                color:'black'
+            }
+
+        },
+        subtitle: {
+            text: 'Number of matches played per year'
+        },
+        xAxis: {
+            type: 'category',
+            labels: {
+                rotation: 0,
+                style: {
+                    fontSize: '13px',
+                    
+                }
+            },
+            title: {
+                text: 'Year',
+                style: {
+                    fontSize: '20px'
+                }
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Number of Match',
+                style: {
+                    fontSize: '20px',
+                }
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            pointFormat: 'Matches Per Year: <b>{point.y}</b>'
+        },
+        series: [{
+            name: 'Matches',
+            data: seriesData,
+            color: '#6F313C',
+            dataLabels: {
+                enabled: true,
+                rotation: 0,
+                color: '#FFFFFF',
+                align: 'right',
+                format: '{point.y}', 
+                y : 30,
+                style: {
+                    fontSize: '17px',
+                }
+            }
+        }]
+    });
+    
+}
+function extraRunsPerTeam(data) {
     let seriesData = [];
     for (let d in data) {
         seriesData.push([d, data[d]]);
